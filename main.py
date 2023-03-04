e536259210bfa02dc242c322ebe8f05f5be77f05
mttga <matteogallici@yahoo.it>
2023-03-04 13:51:03 +0100

    Primo Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+def main():
+    df = pd.read_csv(
+        'datasets/OPSD_Germany_all.csv',
+        parse_dates=True,
+        index_col='Date'
+    )
+    df['Consumption'].plot(figsize=(15, 6),
+                           marker='.',
+                           markersize=3,
+                           linestyle='')
+    plt.title('Consumptions')
+    plt.savefig('figure.png')
+    
+if __name__=='__main__':
+    main()
