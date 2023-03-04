868ce2388f089ea1487275b59ad12829a459c080
Secreti Nicola <Nicola.Secreti@alleanza3-0.coop.it>
2023-03-04 14:06:17 +0100

    Primo Commit

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 def main():
+    #legge il csv
     df = pd.read_csv(
         'datasets/OPSD_Germany_all.csv',
         parse_dates=True,
         index_col='Date'
     )
+    #produce il grafico
     df['Consumption'].plot(figsize=(15, 6),
                            marker='.',
                            markersize=3,
