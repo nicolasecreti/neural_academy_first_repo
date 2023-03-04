49e67389706a375c05b10af3022007f6c16a393a
mttga <matteogallici@yahoo.it>
2023-03-04 14:10:52 +0100

    aggiunta di commenti

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 def main():
+    # legge il csv
     df = pd.read_csv(
         'datasets/OPSD_Germany_all.csv',
         parse_dates=True,
         index_col='Date'
     )
+    # genera il grafico
     df['Consumption'].plot(figsize=(15, 6),
                            marker='.',
                            markersize=3,
                            linestyle='')
-    plt.title('Consumptions Plot')
+    # salva il grafico sul disco
     plt.savefig('figure.png')
     
 if __name__=='__main__':
